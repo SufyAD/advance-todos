364a9e8286639c3d31c368e69a3ce16f0461f422
Sufyan Ahmed <muhammadsufyan20@gmail.com>
2025-02-09 21:17:11 +0500

    add schemas for todos

---
+++ api/schemas/todo.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import Literal, Optional
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+GetTodo = pydantic_model_creator(None, name='Todo')
+
+class PostTodo():
+    task: str = Field(..., max_length=100)
+    done: bool
+    
+class PostTodo(BaseModel):
+    task: Optional[str] = Field(None, max_length=100)
+    done: Optional[bool]
