e5b1d4309d56f6ce2e47d589519a6874cf7c116d
Sufyan Ahmed <muhammadsufyan20@gmail.com>
2025-02-11 23:00:32 +0500

    regsiter tortoise_orm with fastapi todo app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 from api.routes.todo import todo_router
 
 app = FastAPI()
 app.include_router(todo_router)
+
+# ----------- register tortoise DB
+register_tortoise(
+    app=app,
+    db_url="sqlite://db.sqlite3",  # Change this for PostgreSQL, MySQL, etc.
+    modules={"models": ["models.todo"]},  # Ensure this matches your models path
+    generate_schemas=True,  # Auto-generate DB schema
+    add_exception_handlers=True,  # Handles database errors
+)
 
 @app.get('/')
 def index():
